03b9b9c2a85127ab974c4a350efde30a921ef876
GammaFivez <72508380+GammaFivez@users.noreply.github.com>
2022-04-25 23:31:05 +0100

    Final

---
+++ MultiQuiz/app/src/main/java/MultiQuiz/App.java
@@ -0,0 +1,15 @@
+package MultiQuiz;
+//
+ // Only task is to open up the main menu jframe
+import java.awt.Color;
+
+
+ // By Numan
+ //
+public class App {
+    //Creates constructor and opens MenuForm.java's jframe
+    public static void main(String[] args) {
+        MenuForm mnuForm = new MenuForm();
+        mnuForm.setVisible(true);
+    }
+}
---
+++ MultiQuiz/app/src/main/java/MultiQuiz/UserInput.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MultiQuiz;
+ // By Numan
+ // Static class that allows varaible to acessed globally
+public class UserInput {
+    public static class DataStorage{
+      public static String usernameDisplay;
+    }
+}
